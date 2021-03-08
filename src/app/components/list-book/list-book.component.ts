30dfea7905bd71c950542408a7a74c07931b7dfd
aradionov <aradionov@at-consulting.ru>
2021-03-08 23:00:37 +0700

    add гnsubscribe

--- src/app/components/list-book/list-book.component.ts
+++ src/app/components/list-book/list-book.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Book } from '../book/book.interface';
 import { BookService } from '../../services/book-service';
 import { StoreService } from '../../services/store.service';
 import { Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-list-book',
   templateUrl: './list-book.component.html',
 })
-export class ListBookComponent implements OnInit {
+export class ListBookComponent implements OnInit, OnDestroy {
 
   constructor(
     private bookService: BookService,
     private store: StoreService,
     private router: Router,
-  ) { }
+  ) {
+  }
+
   books: Book[];
+  private destroy$ = new Subject();
 
   ngOnInit(): void {
     this.bookService.getBooks()
-      .subscribe(books => this.books = books);
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(books => this.books = books);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   edit(id, event: Event): void {
